edf832c2c1bf1f8c701028fdbe64baa18cdf536c
Don Tian <dontian@Dons-MacBook-Pro.local>
2016-11-26 01:57:35 -0800

    Add functionality for selecting image

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class MemeEditorViewController: UIViewController {
+class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var selectedImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,43 @@ class MemeEditorViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func selectImageFromCamera(_ sender: Any) {
+        selectImageFromSource(UIImagePickerControllerSourceType.camera)
+    }
+
+    @IBAction func selectImageFromAlbum(_ sender: Any) {
+        selectImageFromSource(UIImagePickerControllerSourceType.savedPhotosAlbum)
+    }
+    
+    func selectImageFromSource(_ sourceType: UIImagePickerControllerSourceType) {
+        if (UIImagePickerController.isSourceTypeAvailable(sourceType)) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.sourceType = sourceType
+            imagePicker.delegate = self
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+            showAlert("Source Not Available", message: "This image source is not availabe on your device.")
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            selectedImageView.image = image
+        }
+        
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
+    }
+    
+    func showAlert(_ title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
 
